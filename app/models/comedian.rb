d45739d9822bad728e4aee80169f6722b71a4a2a
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-01 12:08:20 -0600

    Adds validations to comedian model

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,9 @@
 class Comedian < ApplicationRecord
+  
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :thumbnail
+  validates_presence_of :city
   has_many :specials
 
   def number_of_specials
