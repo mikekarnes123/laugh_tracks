eaf7b957b40dd146f9a118bcd3e23064feab2d35
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-02 09:46:11 -0600

    Adds default image logic

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,10 +1,18 @@
 class Comedian < ApplicationRecord
-  
+
   validates_presence_of :name
   validates_presence_of :age
   validates_presence_of :thumbnail
   validates_presence_of :city
   has_many :specials
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    if thumbnail == ""
+      self.thumbnail = "https://cdn140.picsart.com/262128021005212.png?r1024x1024"
+    end
+  end
 
   def number_of_specials
     specials.count
